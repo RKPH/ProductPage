0f3e00e8fa0fe7ddd97548c99ad90e018ee1aee9
RKPH <pnghung2003@gmail.com>
2024-10-28 21:52:49 +0700

    init men page

--- src/Pages/MenPage.jsx
+++ src/Pages/MenPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const MenPage = () => {
-  return <div>MenPage</div>;
+  return (
+    <div>
+      <ul className="h-10 w-ful flex gap-x-4 bg-red-300">
+        <div className="w-10 h-10 bg-yellow-200 "></div>
+      </ul>
+    </div>
+  );
 };
 
 export default MenPage;
