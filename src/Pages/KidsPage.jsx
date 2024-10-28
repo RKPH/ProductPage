8eed7676f0a189e0a9ffe76ab4e113eee8ccb94a
Ea123 <nguyendohaiduy0204@gmail.com>
2024-10-28 21:25:16 +0700

    update kidspage2

--- src/Pages/KidsPage.jsx
+++ src/Pages/KidsPage.jsx
@@ -77,7 +77,7 @@ const KidsPage = () => {
           alt="Kids Banner"
           className="w-full h-[500px] object-cover"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center text-center space-y-4">
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex  items-center justify-center text-center space-y-4">
           <h1 className="text-white text-4xl md:text-6xl font-bold">
             ADIDAS Z.N.E.
           </h1>
@@ -89,7 +89,7 @@ const KidsPage = () => {
 
       {/* Age Range Section */}
       <section className="p-10">
-        <div className="flex flex-col md:flex-row justify-between gap-3">
+        <div className="flex  md:flex-row justify-between gap-3">
           <div className="age-group mb-4 md:mb-0">
             <img
               src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_530,w_530/SS_18_YA_Infants_Q1_Ecomm_A_FC_Age_F_640x640_4b47ce365a.jpg"
@@ -138,7 +138,7 @@ const KidsPage = () => {
           />
         </video>
 
-        <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col items-center justify-center text-center space-y-4">
+        <div className="absolute inset-0 bg-black bg-opacity-30 flex  items-center justify-center text-center space-y-4">
           <h2 className="text-white text-4xl md:text-6xl font-bold">
             Disney Pegasus Collection
           </h2>
