de7a99e2160f901b1f98ea58702c6161e3383607
GiaHung11062003 <hung_pham_nguyen_gia@bita.jp>
2024-11-12 12:04:24 +0700

    fix img size

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -8,11 +8,28 @@ const ProductCard = ({ image, price, name, category }) => {
         <Card className="flex-shrink-0 w-72 rounded-md border overflow-hidden hover:border-black mb-2 shadow-none">
             {/* Product Image */}
             <div className="relative aspect-square flex items-center justify-center bg-gray-100">
-                <img
-                    src={image}
-                    alt={name}
-                    className="object-contain h-full w-full p-2" // Add padding to keep it centered
-                />
+                <picture>
+
+                    <source
+                        srcSet="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/sportswear_fw24_zne_launch_mglp_carousel_mini_lookbook_4_d_b322bb6282.jpg"
+                        media="(min-width: 960px)" width="1050" height="1400"/>
+
+
+                    <source
+                        srcSet="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_300,w_300/sportswear_fw24_zne_launch_mglp_carousel_mini_lookbook_4_t_2471e36eca.jpg"
+                        media="(min-width: 768px)" width="1050" height="1400"/>
+
+
+                    <source
+                        srcSet="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_300,w_300/sportswear_fw24_zne_launch_mglp_carousel_mini_lookbook_4_m_357ee2cb53.jpg"
+                        media="(max-width: 767px)" width="1050" height="1400"/>
+
+
+                    <img
+                        src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/sportswear_fw24_zne_launch_mglp_carousel_mini_lookbook_4_d_b322bb6282.jpg"
+                        alt="" loading="lazy" fetchPriority="auto"/>
+                </picture>
+
             </div>
 
             {/* Product Content */}
